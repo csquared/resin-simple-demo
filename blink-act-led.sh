e607ee30a881ad77ee1704039f172dd81886ef98
csquared <christopher.continanza@gmail.com>
2014-04-28 14:28:59 -0700

    simplest script I could think of

---
+++ blink-act-led.sh
@@ -0,0 +1,32 @@
+# disable activity trigger for led0
+echo none >/sys/class/leds/led0/trigger
+
+#ten fast blinks
+for i in `seq 1 10`;
+do
+  echo 1 >/sys/class/leds/led0/brightness
+  sleep 0.25
+  echo 0 >/sys/class/leds/led0/brightness
+  sleep 0.25
+done
+
+#ten slow blinks
+for i in `seq 1 10`;
+do
+  echo 1 >/sys/class/leds/led0/brightness
+  sleep 1
+  echo 0 >/sys/class/leds/led0/brightness
+  sleep 1
+done
+
+#ten fast blinks
+for i in `seq 1 10`;
+do
+  echo 1 >/sys/class/leds/led0/brightness
+  sleep 0.25
+  echo 0 >/sys/class/leds/led0/brightness
+  sleep 0.25
+done
+
+# re-enable activity trigger
+echo mmc0 >/sys/class/leds/led0/trigger
